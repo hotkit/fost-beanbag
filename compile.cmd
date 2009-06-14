92e4acb2344a07eb1ae1a46a894544175f306586
Kirit Sælensminde <k@kirit.com>
2009-06-14 03:33:41 +0000

    Added in tests for the libraries in fost-orm.

--- compile.cmd
+++ compile.cmd
@@ -1,6 +1,19 @@
-..\bjam toolset=msvc %*
 @echo off
+..\bjam toolset=msvc %*
+
 call ..\boost-version.cmd
 IF NOT ERRORLEVEL 1 (
     for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
+)
+
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\ftest ..\dist\bin\fost-jsondb-test-smoke.dll ..\dist\bin\fost-schema-test-smoke.dll
+)
+
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\fost-schema-test-jsondb-file Cpp\fost-schema-test\jsondb-file.json
+)
+
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\fost-jsondb-test-file
 )
