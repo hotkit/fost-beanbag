af338e3cc0586f559cd14514f0902f6ac9f37c7b
Kirit Sælensminde <k@kirit.com>
2009-08-10 06:01:54 +0000

    Updated to new location of string headers.

--- Cpp/fost-schema-test/database.cpp
+++ Cpp/fost-schema-test/database.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,8 +7,8 @@
 
 
 #include "fost-schema-test.hpp"
-#include <fost/db.hpp>
-#include <fost/string/utility.hpp>
+#include <fost/string>
+#include <fost/db>
 
 #include <fost/thread.hpp>
 
