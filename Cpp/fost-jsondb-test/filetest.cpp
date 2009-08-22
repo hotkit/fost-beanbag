4e3172d721585afc8b9420cc836024ab971402e1
Kirit Sælensminde <k@kirit.com>
2009-08-22 05:26:54 +0000

    Now actually does some tests with the file handling of the JSON database.

--- Cpp/fost-jsondb-test/filetest.cpp
+++ Cpp/fost-jsondb-test/filetest.cpp
@@ -16,11 +16,36 @@
 using namespace fostlib;
 
 
+namespace {
+    const setting< bool > c_keep_file("fost-jsondb-test-file", "fost-jsondb-test-file", "Keep database file", false);
+}
+
+
 FSL_MAIN(
     L"fost-jsondb-test-file",
     L"fost-jsondb-test-file\nTest the file handling for Fost 4 JSON blobs\nCopyright (c) 2008, Felspar Co. Ltd."
 )( fostlib::ostream &out, fostlib::arguments &args ) {
     string dbname( guid() );
+
+    // Put an empty JSON blob in the database
+    {
+        jsondb database( dbname, json() );
+        jsondb::local loc1( database );
+        loc1
+            .insert( jcursor(10), json(true) )
+            .commit()
+        ;
+    }
+
+    // We can now reload that database and remove the item from the end
+    {
+        jsondb database( dbname );
+        jsondb::local loc1( database );
+        loc1
+            .remove( jcursor(10) )
+            .commit()
+        ;
+    }
 
     return 0;
 }
