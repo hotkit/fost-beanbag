54c8397c3e5ea597b5228677dfcca1bced6e3867
Kirit Sælensminde <k@kirit.com>
2009-09-01 03:48:06 +0000

    Added some text to describe what the tests do.

--- Cpp/fost-schema-test/fields.cpp
+++ Cpp/fost-schema-test/fields.cpp
@@ -19,16 +19,19 @@ FSL_TEST_SUITE( fields );
 
 
 FSL_TEST_FUNCTION( define ) {
-    const field_wrapper< int64_t > i( L"int64" );
+    FSL_CHECK_NOTHROW( const field_wrapper< int64_t > i( L"int64" ) );
 }
 
 
 FSL_TEST_FUNCTION( registry ) {
     FSL_CHECK_EQ( field_base::fetch( L"integer" ).type_name(), L"integer" );
+    // zzzz is not a valid type. Should throw an exception
     FSL_CHECK_EXCEPTION( field_base::fetch( L"zzzz" ), exceptions::out_of_range< std::size_t >& );
     {
+        // Registering a second instance of an extant type should make it no longer usable
         const field_wrapper< int64_t > i( L"integer" );
         FSL_CHECK_EXCEPTION( field_base::fetch( L"integer" ), exceptions::out_of_range< std::size_t >& );
     }
+    // Once the new instance goes out of scope then the type becomes usable again
     FSL_CHECK_EQ( field_base::fetch( L"integer" ).type_name(), L"integer" );
 }
