bf0be1be7e7eddd29c09e35d346ff181be4be812
Kirit Sælensminde <k@kirit.com>
2009-10-03 08:27:20 +0000

    Added some basic documentation to the lowest level library in fost-orm.

---
+++ Docs/fost-schema/index.hpp
@@ -0,0 +1,9 @@
+/** \defgroup fost_schema fost-schema
+    \ingroup fost_orm
+
+    \brief fost-schema is the lowest most layer of the OR/M part of Fost 4. It is responsible for handling the abstractions of the databases themselves.
+
+    The database abstraction is able to handle both RDBMS systems as well as more exotic database options. This library includes a simple transactional JSON database with minimum features.
+
+    The abstraction layer is able to handle both dynamic models (i.e. the models are defined at runtime, possibly through introspection of an existing database), static models (the models are defined more or less as normal C++ classes with special annotations) and a mixture of the two (static models augmented with dynamic fields).
+*/
---
+++ Docs/modules.hpp
@@ -0,0 +1,4 @@
+/** \defgroup fost_orm fost-orm
+
+    \brief This package contains implementations of various concrete persistence layers for Fost and the abstractions that allow them to operate together.
+*/
