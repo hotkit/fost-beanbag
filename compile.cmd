ecf099c5028b1abd33bde1991b0fa9c3755abcbc
Kirit Sælensminde <k@kirit.com>
2010-01-04 10:17:23 +0000

    Now builds with new platform macros.

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,19 @@
 @echo off
-echo fost-orm
-..\bjam %* preserve-test-targets=on
+echo fost-orm %*
+pushd %0\..
+
+call ..\boost-version.cmd
+
+IF %BOOST_VERSION_MAJOR% LEQ 39 (
+    ..\bjam preserve-test-targets=on %*
+) ELSE (
+    ..\bjam %*
+)
+
+IF ERRORLEVEL 1 (
+    echo fost-orm %* BUILD FAIL
+    popd
+    copy
+) ELSE (
+    popd
+)
