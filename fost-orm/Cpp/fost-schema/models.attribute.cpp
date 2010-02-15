0ca3432aa6443878b1e3e92bf7eca66a7c9f5086
Kirit Sælensminde <k@kirit.com>
2010-02-15 14:10:31 +0000

    Changed the not_null attribute to be required and added explanations for what more of the design is doing.

--- fost-orm/Cpp/fost-schema/models.attribute.cpp
+++ fost-orm/Cpp/fost-schema/models.attribute.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 1999-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 1999-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -21,7 +21,8 @@ using namespace fostlib;
 fostlib::meta_attribute::meta_attribute(
     const string &name, const field_base &type, bool key, bool not_null,
     const nullable< std::size_t > &size, const nullable< std::size_t > &precision
-) : name( name ), key( key ), not_null( not_null ), size( size ), precision( precision ), m_type( type ) {
+) : name( name ), key( key ), required( not_null ),
+        size( size ), precision( precision ), m_type( type ) {
 }
 
 fostlib::meta_attribute::~meta_attribute() {}
