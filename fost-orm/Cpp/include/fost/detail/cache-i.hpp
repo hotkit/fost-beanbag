fd6d3701b512b1ef4f9c378d4f3353f03b77f35f
Kirit Sælensminde <k@kirit.com>
2010-03-04 16:43:11 +0000

    Started to add some tests for the handling of the object_ptr and started to sketch out the smart pointer handling.

--- fost-orm/Cpp/include/fost/detail/cache-i.hpp
+++ fost-orm/Cpp/include/fost/detail/cache-i.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 1998-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 1998-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -35,6 +35,17 @@ bool fostlib::object_ptr< O, K >::operator != ( fostlib::t_null ) const {
 template< typename O, typename K > inline
 const typename fostlib::object_ptr< O, K >::key_type &fostlib::object_ptr< O, K >::value() const {
     return m_key.value();
+}
+
+template< typename O, typename K > inline
+typename fostlib::object_ptr< O, K >::instance_type *
+        fostlib::object_ptr< O, K >::operator -> () {
+    if ( m_key.isnull() )
+        throw exceptions::null("Pointer does not contain a key");
+    else
+        throw exceptions::not_implemented(
+            "Fetching a pointer when there is a key"
+        );
 }
 
 namespace fostlib {
