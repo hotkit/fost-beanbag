4e10df0564977b38a5cafb3fa8810ca80f132e96
Kirit Sælensminde <k@kirit.com>
2010-04-04 04:27:38 +0000

    * Moved a header into a better location. * Added a change log to better track future changes. * Tidied up some of the testing.

--- fost-orm/Cpp/fost-cache/object-cache.cpp
+++ fost-orm/Cpp/fost-cache/object-cache.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 1998-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 1998-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,7 +7,7 @@
 
 
 #include "fost-cache.hpp"
-#include <fost/detail/cache.hpp>
+#include <fost/cache.hpp>
 
 
 using namespace fostlib;
--- fost-orm/Cpp/fost-cache/object-ptr-tests.cpp
+++ fost-orm/Cpp/fost-cache/object-ptr-tests.cpp
@@ -1,12 +1,12 @@
 /*
-    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
 */
 
 
-#include "fost-cache-test.hpp"
+#include <fost/test>
 #include <fost/cache>
 #include <fost/factory>
 
@@ -30,6 +30,16 @@ FSL_TEST_SUITE( object_ptr );
 
 
 FSL_TEST_FUNCTION( basic_operations ) {
-    fostlib::test::default_copy_constructable< fostlib::object_ptr< Model > >();
+    fostlib::test::default_copy_constructable<
+        fostlib::object_ptr< Model >
+    >();
     fostlib::test::default_isnull< fostlib::object_ptr< Model > >();
+
+    FSL_CHECK_EQ( fostlib::object_ptr< Model >(), fostlib::null );
+}
+
+
+FSL_TEST_FUNCTION( pointer_ops_when_null ) {
+    fostlib::object_ptr< Model > ptr;
+    FSL_CHECK_EXCEPTION(ptr->pk(), fostlib::exceptions::null&);
 }
