20eced6127bfb7f0f76a3f104dfcd081190539ee
Kirit Sælensminde <k@kirit.com>
2010-10-02 09:49:28 +0000

    A failing test now fails for the right reason.

--- fost-orm/Cpp/fost-schema-test/factory-incorrect-model.cpp
+++ fost-orm/Cpp/fost-schema-test/factory-incorrect-model.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2010, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -11,8 +11,8 @@
 
 class BasicModel : public fostlib::model< BasicModel > {
 public:
-    BasicModel( const fostlib::json &j )
-    : model_type( j ){}
+    BasicModel( const fostlib::initialiser &i )
+    : model_type( i ){}
 };
 
 class SubModelType : public BasicModel {
