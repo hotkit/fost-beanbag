812044c0c61433c33b28e08fe6d1cbe69ccc2e27
Kirit Sælensminde <k@kirit.com>
2013-03-10 06:19:31 +0000

    `fostlib::absorbException` has been deprecated in favour of `fostlib::absorb_exception`.

--- fost-orm/Cpp/fost-schema/db.cpp
+++ fost-orm/Cpp/fost-schema/db.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 1999-2010, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 1999-2013,Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -400,7 +400,7 @@ fostlib::recordset::recordset( boost::shared_ptr< dbinterface::recordset > rs )
 fostlib::recordset::~recordset()
 try {
 } catch ( ... ) {
-    absorbException();
+    absorb_exception();
 }
 
 
