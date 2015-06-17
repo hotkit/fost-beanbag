b641b62fcd2c9f438aee7f6167f7d25030b2483e
Kirit Sælensminde <k@kirit.com>
2015-06-17 20:41:53 +0700

    Convert the code to use the `f5::tsmap`.

--- beanbag/databases.cpp
+++ beanbag/databases.cpp
@@ -8,14 +8,15 @@
 
 #include "beanbag.hpp"
 #include <beanbag/databases.hpp>
+#include <f5/threading/map.hpp>
 #include <fost/insert>
 #include <fost/unicode>
 
 
 namespace {
-    typedef std::map< fostlib::string, beanbag::jsondb_ptr > databases_t;
-    boost::mutex g_mutex;
-    databases_t g_databases;
+    f5::tsmap<fostlib::string, boost::weak_ptr<fostlib::jsondb>,
+        f5::weak_ptr_promotion_policy<boost::weak_ptr<fostlib::jsondb>>>
+            g_databases;
 }
 
 
@@ -30,34 +31,44 @@ beanbag::jsondb_ptr beanbag::database(
         else
             name = which_name.value();
 
-        boost::mutex::scoped_lock lock(g_mutex);
-        databases_t::const_iterator loc(g_databases.find(name));
-
-        if ( loc == g_databases.end() ) {
-            if ( !which.has_key("filepath") )
-                throw fostlib::exceptions::null(
-                    "Beanbag configurations must specify a file path in the 'filepath' member");
-            fostlib::json tplate;
-            if ( which.has_key("template") )
-                tplate = fostlib::json::parse(fostlib::utf::load_file(
-                    fostlib::coerce<boost::filesystem::wpath>(which["template"])));
-            else if ( which.has_key("initial") )
-                tplate = which["initial"];
-            else {
-                boost::shared_ptr< fostlib::jsondb > db(
-                    new fostlib::jsondb(
-                        fostlib::coerce<fostlib::string>(which["filepath"])));
-                g_databases[name] = db;
-                return db;
-            }
-            boost::shared_ptr< fostlib::jsondb > db(
-                new fostlib::jsondb(
-                    fostlib::coerce<fostlib::string>(which["filepath"]),
-                    tplate) );
-            g_databases[name] = db;
-            return db;
-        } else
-            return loc->second;
+        beanbag::jsondb_ptr cptr;
+        auto make = [&which, &cptr]() {
+                if ( !which.has_key("filepath") )
+                    throw fostlib::exceptions::null(
+                        "Beanbag configurations must specify a file path in the 'filepath' member");
+                fostlib::json tplate;
+                if ( which.has_key("template") ) {
+                    tplate = fostlib::json::parse(fostlib::utf::load_file(
+                        fostlib::coerce<boost::filesystem::wpath>(which["template"])));
+                } else if ( which.has_key("initial") ) {
+                    tplate = which["initial"];
+                } else {
+                    // Without some sort of template we insist the file exists
+                    cptr =  boost::make_shared<fostlib::jsondb>(
+                            fostlib::coerce<fostlib::string>(which["filepath"]));
+                    return cptr;
+                }
+                // With a template of some sort we can create a new disk file
+                cptr = boost::make_shared< fostlib::jsondb>(
+                        fostlib::coerce<fostlib::string>(which["filepath"]),
+                        tplate);
+                return cptr;
+            };
+        auto fptr = g_databases.add_if_not_found(name, make);
+        if ( cptr ) {
+            // cptr is set if there was a cache miss and the lambda generated
+            // a new value
+            return cptr;
+        } else if ( fptr ) {
+            // fptr is set (and has a value) if we got a cache hit and the
+            // database is still held somewhere
+            return fptr;
+        } else {
+            // There is a dead reference in the table -- do something....
+            // just not something with a race
+            throw fostlib::exceptions::not_implemented(__FUNCTION__,
+                "When we get a dead reference");
+        }
     } catch ( fostlib::exceptions::exception &e ) {
         fostlib::insert(e.data(), "opening-beanbag", which);
         throw;
@@ -72,27 +83,28 @@ void beanbag::alias(const fostlib::string &name, beanbag::jsondb_ptr db) {
 
 
 void beanbag::remove(beanbag::jsondb_ptr db) {
-    { // Don't hold the lock while we hit the file system
-        boost::mutex::scoped_lock lock(g_mutex);
-        for ( databases_t::iterator iter(g_databases.begin()); iter != g_databases.end(); ) {
-            if ( iter->second == db ) {
-                g_databases.erase(iter++); // Note post increment
-            } else {
-                ++iter;
-            }
-        }
-    }
-    if ( not db->filename().isnull() ) {
-        boost::filesystem::remove(db->filename().value());
-    }
+    throw fostlib::exceptions::not_implemented(__FUNCTION__);
+//     { // Don't hold the lock while we hit the file system
+//         boost::mutex::scoped_lock lock(g_mutex);
+//         for ( databases_t::iterator iter(g_databases.begin()); iter != g_databases.end(); ) {
+//             if ( iter->second == db ) {
+//                 g_databases.erase(iter++); // Note post increment
+//             } else {
+//                 ++iter;
+//             }
+//         }
+//     }
+//     if ( not db->filename().isnull() ) {
+//         boost::filesystem::remove(db->filename().value());
+//     }
 }
 
 
 void beanbag::test_database(const fostlib::string &name, const fostlib::json &blob) {
-    jsondb_ptr db(new fostlib::jsondb);
+    jsondb_ptr db(boost::make_shared<fostlib::jsondb>());
     fostlib::jsondb::local transaction(*db);
     transaction.insert(fostlib::jcursor(), blob);
     transaction.commit();
-    alias(name, db);
+    g_databases.insert_or_assign(name, db);
 }
 
