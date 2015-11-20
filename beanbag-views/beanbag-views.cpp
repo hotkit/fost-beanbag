82754c772ea7d3fc4776e950d847ebc93052d4ef
Kirit Sælensminde <k@kirit.com>
2015-11-20 12:56:32 +0700

    Removed use of deprecated Fost APIs.

---
+++ beanbag-views/beanbag-views.cpp
@@ -0,0 +1,18 @@
+/*
+    Copyright 2015, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "beanbag-views.hpp"
+
+
+namespace {
+    const fostlib::module c_fost_beanbag(fostlib::c_fost, "beanbag");
+}
+
+
+const fostlib::module fostlib::c_fost_beanbag_views(c_fost_beanbag, "views");
+
